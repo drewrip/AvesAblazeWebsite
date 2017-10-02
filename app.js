63fd00f5aa7f10608b8d388dd061755d087e22bc
Drew Ripberger <drew.ripberger@gmail.com>
2017-10-02 15:23:07 -0400

    Update app.js

--- app.js
+++ app.js
@@ -4,11 +4,16 @@ const ejs = require('ejs');
 
 
 const app = express();
+app.use(express.static(__dirname + '/views'));
 app.set('views', './views');
 app.set('view engine', 'ejs');
 
 app.get("/", function(req, res){
   res.render("index");
 });
+
+app.post("/aboutUs", function(req, res){
+  res.render("aboutUs");
+})
 
 app.listen(8080);
